88d9f01ced876555fbd30cfa13300a2c7b0d9cb0
yongzx <zhengxin.yong@minerva.kgi.edu>
2017-12-21 15:59:57 -0800

    Call Google Sheets API - Email all the Applicants on the Google Sheets

--- server.py
+++ server.py
@@ -2,77 +2,103 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from intercom.client import Client
 import generic
 import sys
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import string
 
 class HTTPServer_Intercom(BaseHTTPRequestHandler):
-	def _set_headers(self):
-		self.send_response(200)
-		self.send_header('Content-type', 'text')
-		self.end_headers()
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text')
+        self.end_headers()
 
-	def do_GET(self):
-		self._set_headers()
-		intercom = Client(personal_access_token='dG9rOjU5Nzk3ZGMwXzg3ZTRfNGYwYV9hZjBmXzRhZGI0MmMyYWE1NzoxOjA=')
+    def do_GET(self):
+        self._set_headers()
+        # Google Spreadsheets Credentials
+        scope = ['http://spreadsheets.google.com/feeds']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        client = gspread.authorize(creds)
 
-		confirmed = False
-		while not confirmed:
-			while True:
-				try:
-					email = input("Email: ")
-					user = intercom.users.find(email=email)
-					break
-				except:
-					message = sys.exc_info()[1]
-					print(message)
-					print("Please make sure that the email \"{}\" is correct".format(email))
-			admin_name = input("Your Name (Not the Applicant's Name): ")
-			challenge = input("Challenge that is reset: ")
-			reason = input("Reason for resetting (don't start with capital letter): ")
-			msg = generic.GenericChallengeReset(admin=admin_name, challenge=challenge,
-												reason=reason)
-			subject, body = msg.response()["subject"], msg.response()["body"]
+        # Intercom Access Token
+        intercom = Client(personal_access_token='dG9rOjU5Nzk3ZGMwXzg3ZTRfNGYwYV9hZjBmXzRhZGI0MmMyYWE1NzoxOjA=')
 
-			print('\nMessage to be sent: \n===================')
-			print(subject)
-			print(body)
+        # Reset the Applicants in a worksheet of a Google Spreadsheet document
+        spreadsheet_title = input("Title of your spreadsheet (e.g. Fall 2018 Regular Decision Challenge QA): ")
+        worksheet_title = input("Title of your worksheet: ")
+        admin_name = input("What's your Intercom name? ")
+        sheet = client.open(spreadsheet_title).worksheet(worksheet_title)
 
-			confirmed_response = input("Is this the message you want? Type 'Y' if yes, 'N' if no \nResponse: ")
-			if confirmed_response == 'Y':
-				confirmed = True
+        result = sheet.get_all_records()
+        for row, applicant in enumerate(result):
+            if applicant.get("Admin Action Taken", "None") != "None" and applicant.get("Emailed?", "Yes") in (
+            "No", "no"):
+                # reset each applicant
+                applicant_email = applicant.get("Email")
+                try:
+                    user = intercom.users.find(email=applicant_email)
 
-		print("Message sending...")
-		email = intercom.messages.create(**{
-			"message_type": "email",
-			"subject": subject,
-			"body": body,
-			"template": "plain",
-			"from": {
-				"type": "admin",
-				"id": "1482390"
-			},
-			"to": {
-				"type": "user",
-				"id": user.id
-			}
-		})
+                    reason = applicant.get("Reason to Reset")
 
-		if email:
-			print("Success\n\n")
+                    actions = applicant.get("Admin Action Taken", "").split(" ")
+                    translator = str.maketrans('', '', string.punctuation)
+                    actions = [action.translate(translator) for action in actions]
+                    to_reset = {
+                        "Understanding": False,
+                        "Writing": False,
+                        "Math": False,
+                        "Creativity": False,
+                        "Reasoning": False
+                    }
+                    for action in actions:
+                        if action in to_reset:
+                            to_reset[action] = True
 
-	def do_HEAD(self):
-		self._set_headers()
 
-	def do_POST(self):
-		# Doesn't do anything with posted data
-		pass
+                    for challenge in to_reset:
+                        if to_reset[challenge] is True:
+                            msg = generic.GenericChallengeReset(admin=admin_name, challenge=challenge,
+                                                                reason=reason)
+                            subject, body = msg.response()["subject"], msg.response()["body"]
+                            email = intercom.messages.create(**{
+                                "message_type": "email",
+                                "subject": subject,
+                                "body": body,
+                                "template": "plain",
+                                "from": {
+                                    "type": "admin",
+                                    "id": "1482390"
+                                },
+                                "to": {
+                                    "type": "user",
+                                    "id": user.id
+                                }
+                            })
+
+                    sheet.update_acell('K{}'.format(row+2), 'Yes')
+
+                    print("{} is notified about the reset.".format(applicant_email))
+
+                except:
+                    message = sys.exc_info()[1]
+                    print(message)
+                    print("Please make sure that the email \"{}\" is registered on Intercom".format(email))
+
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        pass
 
 
 def run_server():
-	print("Starting server http://127.0.0.1:8080/...")
+    print("Starting server http://127.0.0.1:8080/...")
 
-	server_address = ('127.0.0.1', 8080)
-	httpd = HTTPServer(server_address, HTTPServer_Intercom)
-	print("Running server http://127.0.0.1:8080/...")
-	httpd.serve_forever()
+    server_address = ('127.0.0.1', 8080)
+    httpd = HTTPServer(server_address, HTTPServer_Intercom)
+    print("Running server http://127.0.0.1:8080/...")
+    httpd.serve_forever()
 
 
 run_server()
